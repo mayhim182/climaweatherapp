1c656bb8caeae33efb48d8da099068ae0b26ba00
may182 <220me2402@nitrkl.ac.in>
2023-04-07 17:09:10 +0530

    separated getting location code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:climaweatherapp/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   // const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +10,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+
+  void initState(){
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async{
+    Location location=Location();
+    await location.getCurrentLocation();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: ElevatedButton(
-              onPressed: (){
-
-              },
-              child: Text('Get Location'),
-          ),
-        ),
     );
   }
 }
