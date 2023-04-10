3cb121776784769b7e896644b5dc86035a74aacd
may182 <220me2402@nitrkl.ac.in>
2023-04-10 11:29:58 +0530

    added http package for api call

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:climaweatherapp/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   // const LoadingScreen({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  var url=Uri.parse('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily,alert&appid=3d94cda811980c81b4e170fff8e3396b');
 
   void initState(){
     super.initState();
@@ -22,9 +24,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
   }
 
+  void getData() async{
+    Response response = await get(url);
+    if(response.statusCode==200){
+      print(response.body);
+    }
+    else{
+      print(response.statusCode);
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
     );
   }
