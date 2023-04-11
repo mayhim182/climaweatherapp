e6d7d6486a0c280a5f50e62ea6d2cb6a9ff5506b
may182 <220me2402@nitrkl.ac.in>
2023-04-11 11:26:58 +0530

    first commit of 11 apr

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:climaweatherapp/services/location.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   // const LoadingScreen({Key? key}) : super(key: key);
@@ -27,6 +28,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData() async{
     Response response = await get(url);
     if(response.statusCode==200){
+      String data =response.body;
+      var longi=jsonDecode(data)['lat'];
+      var latit=jsonDecode(data)['lon'];
+      var weatherDesc=jsonDecode(data)['current']['weather'][0]['description'];
+      var temperature=jsonDecode(data)['current']['temp'];
+      var weatherId=jsonDecode(data)['current']['weather'][0]['id'];
+      var timeZone=jsonDecode(data)['timezone'];
       print(response.body);
     }
     else{
@@ -40,6 +48,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     getData();
     return Scaffold(
+
     );
   }
 }
